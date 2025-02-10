6847d3efe12b141aa65698c91e95cf875856aa89
Viktor Stefanov <victorystefanov@gmail.com>
2025-02-10 13:46:38 +0200

    dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,22 +1,24 @@
 import Menu from "@/components/Menu/Menu";
+import NavBar from "@/components/NavBar/NavBar";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function DashboardLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="h-screen flex">
-      <aside className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] transition-[width] duration-300 bg-red-200 pl-4 pt-4">
+      <aside className="h-screen w-[14%] md:w-[8%] lg:w-[16%] xl:w-[6%] transition-[width] duration-300 bg-[#F7F8FA] pl-3 pt-4 flex flex-col">
         <Link href={"/"} className="flex items-center justify-center lg:justify-start gap-2">
-          <Image src="/logo.png" alt="logo" width={32} height={32}/>
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block">Academix</span>
         </Link>
         <Menu />
       </aside>
-      <main className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">RIGHT</main>
+      <main className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[94%] bg-[#F7F8FA] overflow-scroll">
+        <NavBar />
+        {children}
+      </main>
     </div>
   );
 }
